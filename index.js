9587e1661f04ecc2279f7fdab2655e9724d7119c
efierr <erickfierro0413@gmail.com>
2024-06-22 15:09:05 -0400

    1-3 done

--- index.js
+++ index.js
@@ -9,28 +9,47 @@ const newMember = require("./data/new-member");
  * @param {Object} obj -
  * @return {number} The number of keys in the object
  */
-function numberOfKeys(obj) {}
+function numberOfKeys(obj) {
+  return _.keys(obj).length;
+}
+console.log(numberOfKeys({a:4,d:4,e:2}));
 
 /**
  * Remove the falsy values in a numbers array and return the sum
  * @param {numbers[]} array - An array of numbers that can also contain some falsy values
  * @return {number} The sum of the numbers in an array
  */
-function sumNumbers(array) {}
+function sumNumbers(array) {
+  return array.filter(Boolean).reduce((sum, num) => sum + num, 0);
+}
 
 /**
  * Convert a two-dimensional array of new member data (each inner array having two values: the first being the key, the second being the value) into an object
  * @param {Array[]} member -
  * @return {number} The sum of the numbers in an array
  */
-function newMemberArrayToObject(member) {}
+function newMemberArrayToObject(member) {
+  return member.reduce((obj, [key, value]) => {
+    obj[key] = value;
+    return obj;
+  }, {})
+}
 
 /**
  * Return an array of objects that grouped by instructors from the classes array of objects
  * @param {Object[]} collection - an array of yoga class objects
  * @return {Object[]} - the reshaped collection where the classes are grouped by instructor name
  */
-function groupClassByInstructor(collection) {}
+function groupClassByInstructor(collection) {
+  return collection.reduce((result, yogaClass) => {
+    const instructor = yogaClass.instructor;
+    if (!result[instructor]) {
+      result[instructor] = [];
+    }
+    result[instructor].push(yogaClass);
+    return result;
+  }, {});
+}
 
 /**
  * Remove the age key from the members array of object
